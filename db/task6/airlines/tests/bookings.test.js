6c2dced72302e4653baf3c1accdd641c41e69d8c
PeachMood <vas140302@gmail.com>
2023-06-07 18:25:58 +0700

    feat: завершить написани тестов

--- db/task6/airlines/tests/bookings.test.js
+++ db/task6/airlines/tests/bookings.test.js
@@ -3,7 +3,7 @@ const app = require('../app.js');
 const request = supertest(app);
 
 describe('Testing endpoints for bookings resource', () => {
-  it('POST "/bookings" should return created booking with ticket number and correct status', async () => {
+  it('POST "/bookings" should return for correct response created booking with ticket number and correct status', async () => {
     const booking = {
       flightNo: 'PG0222',
       flightDate: '2017-08-20',
@@ -16,6 +16,59 @@ describe('Testing endpoints for bookings resource', () => {
       expect(response.headers['content-type']).toMatch('application/json');
       expect(response.body).toHaveProperty('bookRef');
       expect(response.body).toHaveProperty('ticketNo');
+    })
+  });
+
+  it('POST "/bookings" should return for incorrect request body BadRequest error with message', async () => {
+    const booking = {
+      flightNo: null,
+      flightDate: null
+    };
+    return request.post('/bookings').send(booking).then((response) => {
+      expect(response.status).toBe(400);
+      expect(response.body.message).toBeDefined();
+    })
+  });
+
+  it('POST "/bookings" should return for arrived flight BadRequest error with message', async () => {
+    const booking = {
+      flightNo: 'PG0403',
+      flightDate: '2017-06-13',
+      fareConditions: 'Economy',
+      passengerId: '_01',
+      passengerName: 'Test user',
+    };
+    return request.post('/bookings').send(booking).then((response) => {
+      expect(response.status).toBe(400);
+      expect(response.body.message).toBeDefined();
+    })
+  });
+
+  it('POST "/bookings" should return BadRequest error with message if there are no available seats for the flight', async () => {
+    const booking = {
+      flightNo: 'PG0222',
+      flightDate: '2017-08-20',
+      fareConditions: 'Business',
+      passengerId: '_01',
+      passengerName: 'Test user',
+    };
+    return request.post('/bookings').send(booking).then((response) => {
+      expect(response.status).toBe(400);
+      expect(response.body.message).toBeDefined();
+    })
+  });
+
+  it('POST "/bookings" should return for non-existing flight NotFound error with message', async () => {
+    const booking = {
+      flightNo: '000000',
+      flightDate: '2017-08-20',
+      fareConditions: 'Economy',
+      passengerId: '_01',
+      passengerName: 'Test user',
+    };
+    return request.post('/bookings').send(booking).then((response) => {
+      expect(response.status).toBe(404);
+      expect(response.body.message).toBeDefined();
     })
   });
 });
--- db/task6/airlines/tests/flights.test.js
+++ db/task6/airlines/tests/flights.test.js
@@ -3,7 +3,7 @@ const app = require('../app.js');
 const request = supertest(app);
 
 describe('Testing endpoints for flights resource', () => {
-  it('POST "/flights/checkins" should return created boarding pass with seat number and correct status', async () => {
+  it('POST "/flights/checkins" should return for correct request created boarding pass with seat number and correct status', async () => {
     const booking = {
       flightNo: 'PG0222',
       flightDate: '2017-08-16',
@@ -17,11 +17,43 @@ describe('Testing endpoints for flights resource', () => {
         ticketNo: booking.ticketNo,
         flightId: 1844
       };
-      return request.post('/flights/checkins').send(boardingPass).then(response => {
+      request.post('/flights/checkins').send(boardingPass).then(response => {
         expect(response.status).toBe(201);
         expect(response.headers['content-type']).toMatch('application/json');
         expect(response.body).toHaveProperty('boardingNo');
         expect(response.body).toHaveProperty('seatNo');
+      });
+    });
+  });
+
+  it('POST "/flights/checkins" should return BadRequest error with message when trying to re-check in for a flight', async () => {
+    const boardingPass = {
+      ticketNo: 'f400a56d43a84',
+      flightId: 1844
+    };
+    return request.post('/flights/checkins').send(boardingPass).then(response => {
+      expect(response.status).toBe(400);
+      expect(response.body.message).toBeDefined();
+    });
+  });
+
+  it('POST "/flights/checkins" should return for non-existing flight NotFound error with message', async () => {
+    const booking = {
+      flightNo: 'PG0222',
+      flightDate: '2017-08-16',
+      fareConditions: 'Economy',
+      passengerId: '_01',
+      passengerName: 'Test user',
+    };
+    return request.post('/bookings').send(booking).then((response) => {
+      const booking = response.body;
+      const boardingPass = {
+        ticketNo: booking.ticketNo,
+        flightId: 9999
+      };
+      return request.post('/flights/checkins').send(boardingPass).then(response => {
+        expect(response.status).toBe(404);
+        expect(response.body.message).toBeDefined();
       });
     });
   });
